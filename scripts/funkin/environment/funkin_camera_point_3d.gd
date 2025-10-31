3db85e894aeb6f430995475952d497db727f0eac
anniebuue <anniebuue@proton.me>
2025-10-31 04:51:55 -0500

    Scripts: Remove previous Funkin' scripts

--- scripts/funkin/environment/funkin_camera_point_3d.gd
+++
@@ -1,8 +0,0 @@
-## Tells FunkinCamera3D where to point to and how to do so.
-class_name FunkinCameraPoint3D extends Marker2D
-
-## Tell the camera if it should assign a zoom specific to this point or not.
-@export var use_custom_zoom : bool = false
-
-## What zoom to apply when focusing on this point.
-@export var custom_zoom : float = 0.0
